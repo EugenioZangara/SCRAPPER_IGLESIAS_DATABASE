7a35a6349a0bb5452cf7f07fb52de2b807f6ecb6
Eugenio <eugeniozangara@gmail.com>
2026-06-26 13:33:59 -0300

    fix(settings): update default SITE_URL to include 'www' for consistency fix(template): update Open Graph and canonical URLs to use SITE_URL variable

--- core/settings.py
+++ core/settings.py
@@ -91,7 +91,7 @@ LOGIN_URL = "/accounts/login/"
 
 FACEBOOK_APP_ID = env("FACEBOOK_APP_ID", default="")
 RESEND_API_KEY = env("RESEND_API_KEY", default="")
-SITE_URL = env("SITE_URL", default="https://parroguia.com")
+SITE_URL = env("SITE_URL", default="https://www.parroguia.com")
 GOOGLE_SITE_VERIFICATION = env("GOOGLE_SITE_VERIFICATION", default="")
 
 IMAGEKIT_PUBLIC_KEY   = env("IMAGEKIT_PUBLI_KEY",    default="")
